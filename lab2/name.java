2360f8b26511e3ee4acbe373a65500c0c7180392
user <user@EB06>
2020-09-29 09:39:53 +0800

    lab1 hw

---
+++ lab2/name.java
@@ -0,0 +1,12 @@
+package lab2;
+
+public class name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am HUANG JIE HAN");
+		System.out.println("MY nid is D0886083");
+		System.out.println("This is lab1 hw");
+	}
+
+}
